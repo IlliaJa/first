c1ae766bf957398854521fc6d015dd4aab3f7eb5
IlliaYaschenko <illyaillya2906@gmail.com>
2018-02-22 21:02:57 +0200

    LinkedList

--- List.js
+++ List.js
@@ -1,77 +1,154 @@
 'use strict';
 
+class Node {
+  constructor(list, data) {
+    this.list = list;
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
 class List {
   constructor() {
     this.last = null;
+    this.first = null;
+
   }
+
   push(data) {
   	const element = new Node(this, data);
-    const prev = this.last;
-    element.prev = this.last;
-    this.last = element;
-    // element.prev.next = element;
+    if (this.last) {
+      const prev = this.last;
+      element.prev = prev;
+      this.last = element;
+      prev.next = element;
+      // console.log('hello')
+    } else {
+      this.last = element;
+      this.first = element;
+      // console.log('hello')
+    }
     return element;
   }
+
   pop() {
-  	this.last = this.last.prev;
-  	this.last.prev.next = null;
+    if(this.last !== this.first) {
+    const prev = this.last.prev;
+  	this.last = this.last.prev; 
+    prev.next = null;
+    } else {
+      this.last = null;
+      this.first = null;
+    }
   }
-  [Symbol.iterator]() {
-  	const element = this.first;
-  	return {
-  		сurrent: element,
-  		next() {
-  			 this.current = element;
-  			 if (!element) {
-  			 	return {
-  				  value: null,
-  				  done: true
-  				};
-  			} else {
-  				return {
-  					value: element.data,
-  					done: false
-  				};
-  			}
-  		}
-  	};
-  }
+
   lenght() {
   	let l = 0;
   	let a = {};
-  	if (this.last != null) {
+  	if (this.last !== null) {
   		a = this.last;
   		l++;
-  	}
-  	while (a.prev != null) {
-  	  a = a.prev;
-  	  l++;
+      while (a.prev !== null) {
+      a = a.prev;
+      l++;
+      }
   	}
   	return l;
   }
-  insert(index, data) {
-  	let i = 0;
-  	let element = this.last;
-  	for (i; i < this.lenght() - index; i++) {
-  	  element = element.prev;
-  	}
-    let prev = element.prev || null;
-  	prev.next = data;
-  	element.prev = data;
-    data.next = element
-    return element;
+
+  [Symbol.iterator]() {
+     let element = this.last;
+     for (let i = 0; i < this.lenght() - 1; i++) {
+      element = element.prev;
+    }
+   //  console.dir(element, {depth: 41})
+    return {
+      сurrent: element,
+      next() {
+         this.current = element;
+         if (!element) {
+          return {
+            value: null,
+            done: true
+          };
+        } else {
+          return {
+            value: element.data,
+            done: false
+          };
+        }
+      }
+    };
   }
+
+  insert(index, data) {
+    const ins = new Node(this, data);
+  	let element = this.last;
+    function  check (prev,ins, first) {
+      if(!prev) {
+        this.first = ins;
+      } else {
+        prev.next = ins;
+        ins.prev = prev;
+        }
+    }
+    if(index - 1 === this.lenght()) {
+      let prev = element;
+      check(prev,ins, this.first);
+      this.last = ins;
+      return ins;
+    } else {
+      for (let i = 0; i < this.lenght() - index; i++) {
+      element = element.prev;
+    }
+    let prev = element.prev;
+    check(prev,ins, this.first);
+    element.prev = ins;
+    ins.next = element;
+    return element;
+    }
+  	
+  }
+
   delete(index) {
     let i = 0;
     let element = this.last;
     for (i; i < this.lenght() - index; i++) {
       element = element.prev;
     }
-    let prev = element.prev || null;
-    let next = element.next || null;
-    if(prev) prev.next = next;
-    if(next) next.prev = prev;
+    const prev = element.prev;
+    const next = element.next;
+    if (prev) prev.next = next;
+    if (next) next.prev = prev;
+    if(index === this.lenght()) {
+  
+      this.last = this.last.prev;
+    }
+    if(index === 1) this.first = this.first.next;
     return element;
+  }
+
+  clone() {
+    let newlist = new List();
+    let element = this.first;
+    while(element) {
+      newlist.push(element.data);
+      element = element.next
+    }
+    return newlist; 
+  }
+  tostring() {
+    let element, next, prev;
+    let string = "";
+    element = this.first;
+    for(let i = 0; i < this.lenght(); i++) {
+    let data = element.data;
+    element = element.next;
+    string += data + ", ";
+    }
+    console.dir(string, {depth: 100})
+    return string;
   }
 }
 
@@ -85,17 +162,14 @@ const obj4 = { name: 'forth' };
 const list = new List();
 list.push(obj1);
 list.push(obj2);
-list.push(obj4);
-list.insert(3, obj3);
-list.pop();
-console.log(list)
-// for (const element of list) {
-//   console.log(element);
-// }
+list.push(obj3);
+list.insert(4, obj4);
+// list.pop();
+// list.delete(1);
+let list1 = list.clone();
+console.dir(list1, {depth: 30})
 
-function Node(list, data) {
-  this.list = list;
-  this.data = data;
-  this.next = null;
-  this.prev = null;
-}
+
+// for (const element of list) {
+//   console.log('hello');
+// }
